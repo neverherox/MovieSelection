003c6681ea07e1be4d4ffe6106e18f3fc2402d50
Kiryl_Ramashkevich <Kiryl_Ramashkevich@epam.com>
2022-04-26 14:31:08 +0300

    delete Actor referece in MovieActor entity

---
+++ MovieSelection.Data/Migrations/20220426112946_DeleteActorInMovieActor.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieSelection.Data.Migrations
+{
+    public partial class DeleteActorInMovieActor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MovieActors_Actors_ActorId",
+                table: "MovieActors");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MovieActors_ActorId",
+                table: "MovieActors");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_MovieActors_ActorId",
+                table: "MovieActors",
+                column: "ActorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MovieActors_Actors_ActorId",
+                table: "MovieActors",
+                column: "ActorId",
+                principalTable: "Actors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}

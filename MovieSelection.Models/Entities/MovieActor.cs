162a03e8510edd7375687ce4da63e902bba4ce5f
Kiryl_Ramashkevich <Kiryl_Ramashkevich@epam.com>
2022-05-03 17:27:59 +0300

    add many to many movie entity

--- MovieSelection.Models/Entities/MovieActor.cs
+++ MovieSelection.Models/Entities/MovieActor.cs
@@ -4,6 +4,8 @@ public class MovieActor
 {
     public int MovieId { get; set; }
 
+    public Movie Movie { get; set; }
+
     public int ActorId { get; set; }
 
     public Actor Actor { get; set; }
